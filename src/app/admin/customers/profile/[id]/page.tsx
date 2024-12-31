22f9617c5bd98e7d06ce78d8cc76443ae0b3283f
Auspicious <info@auspicioussoft.com>
2024-12-31 16:45:16 +0530

    Refactor profile page component for improved readability and maintainability

--- src/app/admin/customers/profile/[id]/page.tsx
+++ src/app/admin/customers/profile/[id]/page.tsx
@@ -14,16 +14,16 @@ import { useTranslations } from "next-intl";
 
 
 const Page = () => {
-   const t = useTranslations('ProfilePage'); 
-  const {id} = useParams();
+  const t = useTranslations('ProfilePage');
+  const { id } = useParams();
   const [query, setQuery] = useState('');
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const {data, error, mutate, isLoading} = useSWR(`/admin/users/${id}`, getSingleUser)
+  const { data, error, mutate, isLoading } = useSWR(`/admin/users/${id}`, getSingleUser)
   const customerData = data?.data?.data;
   const associatedProjects = customerData?.projects
- 
+
   const [formData, setFormData] = useState<any>({
-     fullName: "",
+    fullName: "",
     phoneNumber: "",
     email: "",
     address: "",
@@ -41,7 +41,7 @@ const Page = () => {
       });
     }
   }, [customerData]);
-  
+
 
   const handleInputChange = (
     e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>
@@ -49,22 +49,22 @@ const Page = () => {
     const { name, value } = e.target as HTMLInputElement & { files: FileList };
     setFormData({
       ...formData,
-      [name]: name === "phoneNumber" ? value : value, 
+      [name]: name === "phoneNumber" ? value : value,
     });
   };
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-  
+
     startTransition(async () => {
       try {
-        const response = await updateSingleUser( `/admin/users/${id}`,formData); 
+        const response = await updateSingleUser(`/admin/users/${id}`, formData);
         if (response?.status === 200) {
-        setIsModalOpen(false);
-        mutate()
+          setIsModalOpen(false);
+          mutate()
           //setNotification("User Added Successfully");
-           toast.success("User details updated successfully");
-          
+          toast.success("User details updated successfully");
+
         } else {
           toast.error("Failed to add User Data");
         }
@@ -73,19 +73,19 @@ const Page = () => {
         toast.error("An error occurred while adding the User Data");
       }
     });
-    
+
   };
-  return ( 
+  return (
     <div>
       <h2 className="section-title text-[#3C3F88]"> {t('clientDetails')}</h2>
       <div className=" bg-white rounded-[10px] md:rounded-[30px] w-full py-[30px] px-[15px] md:p-10 ">
         <div className="mb-10 flex gap-[20px] justify-between ">
           {/* src={formData.profilePic || imgNew}  */}
-            <Image src={imgNew} alt="hjfg" height={200} width={200} className="max-w-[100px] md:max-w-[200px] aspect-square rounded-full  " />           
-        <div> 
-          <button  onClick={() => setIsModalOpen(true)} className="w-full !rounded-[3px] button !h-[40px] "> 
-          <EditButtonIcon/> {t('editDetails')}
-        </button></div>
+          <Image src={imgNew} alt="hjfg" height={200} width={200} className="max-w-[100px] md:max-w-[200px] aspect-square rounded-full  " />
+          <div>
+            <button onClick={() => setIsModalOpen(true)} className="w-full !rounded-[3px] button !h-[40px] ">
+              <EditButtonIcon /> {t('editDetails')}
+            </button></div>
         </div>
         <div className="fomm-wrapper grid md:flex flex-wrap gap-5 ">
           <div className="w-full">
@@ -136,6 +136,10 @@ const Page = () => {
       </div>
 
       <EditClientDetailsModal
+        isPending={isLoading}
+        profilePic=""
+        setFormData={setFormData}
+        
         isOpen={isModalOpen}
         onClose={() => setIsModalOpen(false)}
         formData={formData}
